d6af28c1cd27a2777607dcda85d9321960f80492
Mstf000 <mostafacrash400@gmail.com>
2024-06-20 17:42:42 +0300

    added product service

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,20 @@
+// src/product/product.service.ts
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../prisma/prisma.service';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+    constructor(private prisma: PrismaService) { }
+
+    async createProduct(data: { name: string; description?: string; price: number; stock: number }) {
+        return this.prisma.product.create({
+            data,
+        });
+    }
+
+    async getProduct(productId: number) {
+        return this.prisma.product.findUnique({
+            where: { productId },
+        });
+    }
+}
