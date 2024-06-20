37f037ecd663e79b1cb8c9e5599552cf159cede4
Mstf000 <mostafacrash400@gmail.com>
2024-06-20 17:45:06 +0300

    added order service

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,4 +1,34 @@
+// src/order/order.service.ts
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../prisma/prisma.service';
 
 @Injectable()
-export class OrderService {}
+export class OrderService {
+    constructor(private prisma: PrismaService) { }
+
+    async createOrder(data: { userId: number; productIds: number[]; status: string }) {
+        return this.prisma.order.create({
+            data: {
+                userId: data.userId,
+                status: data.status,
+                products: {
+                    connect: data.productIds.map((id) => ({ productId: id })),
+                },
+            },
+        });
+    }
+
+    async getOrder(orderId: number) {
+        return this.prisma.order.findUnique({
+            where: { orderId },
+            include: { products: true },
+        });
+    }
+
+    async updateOrderStatus(orderId: number, status: string) {
+        return this.prisma.order.update({
+            where: { orderId },
+            data: { status },
+        });
+    }
+}
