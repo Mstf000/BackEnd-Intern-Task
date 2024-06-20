d35aa4937999ce38e12ebfeffa7d2825b3486d49
Mstf000 <mostafacrash400@gmail.com>
2024-06-20 17:46:46 +0300

    added order controller

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+// src/order/order.controller.ts
+import { Controller, Get, Post, Put, Body, Param } from '@nestjs/common';
+import { OrderService } from './order.service';
 
 @Controller('order')
-export class OrderController {}
+export class OrderController {
+    constructor(private readonly orderService: OrderService) { }
+
+    @Post()
+    async createOrder(@Body() data: { userId: number; productIds: number[]; status: string }) {
+        return this.orderService.createOrder(data);
+    }
+
+    @Get(':id')
+    async getOrder(@Param('id') id: string) {
+        return this.orderService.getOrder(Number(id));
+    }
+
+    @Put(':id/status')
+    async updateOrderStatus(@Param('id') id: string, @Body() status: string) {
+        return this.orderService.updateOrderStatus(Number(id), status);
+    }
+}
