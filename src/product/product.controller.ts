06389c29f2cf4cc2de70830b3c09c5d9c2d00cf6
Mstf000 <mostafacrash400@gmail.com>
2024-06-20 17:43:22 +0300

    added prodcut controller

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+// src/product/product.controller.ts
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { ProductService } from './product.service';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+    constructor(private readonly productService: ProductService) { }
+
+    @Post()
+    async createProduct(@Body() data: { name: string; description?: string; price: number; stock: number }) {
+        return this.productService.createProduct(data);
+    }
+
+    @Get(':id')
+    async getProduct(@Param('id') id: string) {
+        return this.productService.getProduct(Number(id));
+    }
+}
