cdc141beace2e4cc0c6c6b794feaa32ad3919bed
Mstf000 <mostafacrash400@gmail.com>
2024-06-20 17:42:09 +0300

    added product module

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,9 +1,12 @@
+// src/product/product.module.ts
 import { Module } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
+import { PrismaModule } from '../../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [ProductService],
-  controllers: [ProductController]
+  controllers: [ProductController],
 })
-export class ProductModule {}
+export class ProductModule { }
